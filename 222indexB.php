26f6ce1ad9ccc0c4e8cddf2ec6bec0d75e19b0e8
Christian Talbott <talbott.christian@gmail.com>
2015-03-27 08:09:45 -0500

    Redone The "Next ver"
    Yay!

--- 222indexB.php
+++ 222indexB.php
@@ -1,14 +1,26 @@
-<!--
-221indexB.html demonstrates basic JavaScript and CSS
-Unpublished work (c)2013 Project Lead The Way
-CSE Activity 2.2.1 HTML5andJavaScript
-Version 12/19/2013 
--->
-
-<html>
-<head>
+<?php
+/**
+ * CSE Activity 2.2.2 IntroducingPHP
+ * 
+ * 222indexB.php models use of PHP in conjunction with MySQL, JavaScript, and CSS
+ * @copyright Unpublished work 2013 Project Lead The Way
+ * @version 2014.7.30
+ */
+ 
+/* 
+This block allows our program to access the MySQL database.
+Elaborated on in 2.2.3.
+ */
+require_once 'teacherdb.php';
+$db_server = mysql_connect($host, $username, $password);
+if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
+mysql_select_db($dbname)
+	or die("Unable to select database: " . mysql_error());
+	
+echo "<script type='text/javascript' src='222popouts.js'></script>";
+echo <<<_END
 	<style = "text/css">
-		#img1popout {
+		#popout {
 			z-index: 5;
 			position: absolute;
 			width: 550px;
@@ -16,105 +28,95 @@ Version 12/19/2013
 			top: 50px;
 			border: 1px dashed #000099;
 			background-color: #ddddff;
-		}
-		#img1popin{}
-		
+		}	
 	</style>
-	<script type = "text/javascript">
-		function initPopout()
-		{
-			var thumb1 = document.getElementById("image1"); 
-			
-			thumb1.onmouseover = showDetailedView;
-		}
+_END;
+	
+// $_COOKIE is a data structure that holds all cookies for this site.
+// This conditional verifies that the cookie 'username' contains data.
+// That would symbolize that the user is logged in as an artist.
+if (isset($_COOKIE['username']))
+{
+	$username = $_COOKIE['username']; // Retrieves the value of the cookie
+	
+	// Dynamically respond to the data in the cookie to recognize the user
+	// An echo statement is used to display something in PHP. Plain text
+	// shows up as such, and html code enclosed in quotes functions as normal html.
+	echo "Welcome back, " . $username . 
+		", click <a href='222artist_portalB.php'>here to go to the Artist's page</a>.<br />";
+	echo "Click <a href='222logoutB.php'>here to Log Out</a>...<br />";
+	echo "<br /><br />Enter information in the fields at the bottom of the page to search the image database.<br /><br />";
+}
+else
+{
+	echo "Click <a href='222artist_portalB.php'>here to log in as an artist</a>.<br />";
+	echo "Or click <a href='222account_creationB.php'>here to create an artist account</a>.<br />";
+}
+$query = "SELECT * FROM artists WHERE firstname='" . $_POST['firstname'] . "' OR lastname='" . $_POST['lastname'] . "'";
+$result_artist = mysql_query($query);
+$row = mysql_fetch_row($result_artist);
+$artistID = $row[0];
+$firstName = $row[2];
+$query = "SELECT * FROM images WHERE username='" . $artistID . "'";
+$image_info_table = mysql_query($query);
+// Call a function defined later in this file, with two arguments
+display_table($artistID, $image_info_table, $dbname, $firstName);
 
-		function showDetailedView()
-		{
-			var im1 = document.getElementById("img1popin");
-			im1.id = "img1popout";
-			im1.innerHTML = "<br /><TABLE><TR><TH rowspan='3'><img src='aprilla/pic1.jpg' width='250'><TH align='left'>Artist's Name: <TH align='left'>Aprill Aronie<TR><TH align='left'>File Name: <TH align='left'>pic1.jpg<TR></TABLE> ";
-			var popOut = document.getElementById("img1popout");
-			popOut.onmouseout = hideDetailedView;			
-		}
+// HTML to display the form on this page.
+echo '<br />Search the art database using the fields below.';
+// Sets POST as method of data submission
+echo '<form action="222indexB.php" method="post"><pre>'; 
+echo 'First Name <input type="text" name="firstname" />';
+echo '<br />Last Name <input type="text" name="lastname" />';
+// Creates the SEARCH button which calls the POST method with the data entered
+echo '<br /><input type="submit" value="SEARCH" />'; 
+echo '</pre></form>';
 
-		function hideDetailedView()
+/** 
+ * Generates HTML to render table of images returned by user query
+ * 
+ * An example of code reuse. This code is needed for each of our form submission cases.
+ * @param string $artistID the name of the artist's folder
+ * @param array $image_info_table a 2D array containing the data about each relevant image.
+ * @return null
+ */
+function display_table($artistID, $image_info_table, $dbname, $firstName)
+{
+	echo "<TABLE><CAPTION>Your Results:</CAPTION>";
+	$closed_tr = 0; // flag, used to determine if we are at the end of a row when the loop terminates
+	$num_images = mysql_num_rows($image_info_table);
+	
+	if ($image_info_table)
+	{
+		// Iterate through all of the returned images, placing them in a table for easy viewing
+		for ($count = 0; $count < $num_images; $count++)
 		{
-			var im1 = document.getElementById("img1popout");
-			im1.id = "img1popin";
-			im1.innerHTML = "";
-		}jake town 
-	</script>
-</head>
-<body onload="initPopout();">
-<br>
-<table>
-	<caption>Your Results:</caption>
-	<tbody>
-	<tr>
-		<td>
-			<img id="image1" src="aprilla/pic1thumb.jpg"><div id = "img1popin"></div>
-		</td>
-		<td>
-			<img src="aprilla/pic2thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic3thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic4thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic5thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic6thumb.jpg">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<img src="aprilla/pic7thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic8thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic9thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic10thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic11thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic12thumb.jpg">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<img src="aprilla/pic13thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic14thumb.jpg">
-		</td>
-		<td>
-			<img src="aprilla/pic15thumb.jpg">
-		</td>
-	</tr>
-	</tbody>
-</table>
-<br>
-Search the art database using the fields below.
-<form method="post" action="index1.html">
-	<pre>
-First Name
-<input type="text" name="firstname">
-<br>
-Last Name
-<input type="text" name="lastname">
-<br>
-<input type="submit" value="SEARCH">
-	</pre>
-</form>
-</body>
-</html>
+			// The following few lines store information from specific cells in the data about an image
+			$image_row = mysql_fetch_row($image_info_table); // Advances a row each time it is called
+			$image_name = $image_row[1];
+			$thumb_name = $image_row[2];
+			$image_path = pathinfo($image_name);
+			$id_name = $image_path['filename'];
+			$div_id = $id_name . "popin";
+
+			// Remember the mod operator, this one gives us the remainder when $count is divided by 6
+			if ($count % 6 == 0)
+			{
+				echo "<TR>";
+				$closed_tr = 0;
+			}
+			$domain = $_SERVER['SERVER_NAME'];
+			echo "<TD><img id='$id_name' src='http://$domain/~$dbname/$artistID/$thumb_name' onmouseover=" . '"' . "
+						showDetailedView('$div_id', '$firstName', '$dbname')" . '" />';
+			echo "<div id = '$div_id'></div></TD>";
+			if ($count % 6 == 5)
+			{
+				echo "</TR>";
+				$closed_tr = 1;
+			}
+		}
+	}
+	if ($closed_tr == 0) echo "</TR>"; // Appends a close tag for the TR element if the loop did not terminate at a row end.
+	echo "</TABLE>";
+}
+?>
